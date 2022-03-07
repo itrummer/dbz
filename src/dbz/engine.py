70d61d0a4f0f1d0eeb4c01eab672800183a1c354
itrummer <immanuel.trummer@gmail.com>
2022-03-06 22:26:44 -0500

    Nearly finished query to code translation.

--- src/dbz/engine.py
+++ src/dbz/engine.py
@@ -30,6 +30,139 @@ class Engine():
         """
         return self.library + self._plan_code(plan)
     
+    def _agg_code(self, agg):
+        """ Generates code for processing aggregates.
+        
+        Args:
+            agg: produce code for this aggregate
+        
+        Returns:
+            code processing given aggregate
+        """
+        pass
+    
+    def _assignment(self, step, op_code):
+        """ Returns code for assigning operation result to variable.
+        
+        Args:
+            step: processing step
+            op_code: code for processing operation
+        
+        Returns:
+            code assigning operation result to variable
+        """
+        result = self._result_name(step['id'])
+        return f'{result} = {op_code}\n' +\
+            f'last_result = {result}'
+    
+    def _binary_code(self, operation):
+        """ Translates binary operation into code.
+        
+        Args:
+            operation: a binary operation
+        
+        Returns:
+            code realizing the operation
+        """
+        op_kind = operation['op']['kind']
+        operands = operation['operands']
+        left_op = self._operation_code(operands[0])
+        right_op = self._operation_code(operands[1])
+        return f'{left_op} {op_kind} {right_op}'
+    
+    def _column_code(self, column_ref):
+        """ Generate code retrieving column of last result. 
+        
+        Args:
+            column_ref: describes column to retrieve from previous result
+        
+        Returns:
+            code retrieving specified column
+        """
+        column_nr = column_ref['input']
+        return f'last_result.get_column({column_nr})'
+    
+    def _literal_code(self, literal):
+        """ Produces a code snippet producing given literal.
+        
+        Args:
+            literal: translate this literal into code
+        
+        Returns:
+            code producing given literal
+        """
+        return literal['literal']
+    
+    def _LogicalAggregate(self, step):
+        """ Produce code for aggregates (optionally with grouping). 
+        
+        Args:
+            step: plan step representing aggregates
+        
+        Returns:
+            code realizing aggregates
+        """
+        step_id = step['id']
+        result = self._result_name(step_id)
+        parts = []
+        parts += [f'{result} = Table()']
+        aggs = step['aggs']
+        groups = step['group']
+        for agg in aggs:
+            agg_code = self._agg_code(agg)
+        return '\n'.join(parts)
+    
+    def _LogicalFilter(self, step):
+        """ Produce code for a filter.
+        
+        Args:
+            step: plan step representing filter
+        
+        Returns:
+            code realizing filter
+        """
+        condition = step['condition']
+        pred_code = self._operation_code(condition)
+        op_code = f'LogicalFilter(last_result, {pred_code})'
+        return self._assignment(step, op_code)
+    
+    def _LogicalJoin(self, step):
+        """ Produce code for an equality join.
+        
+        Args:
+            step: plan step representing join
+        
+        Returns:
+            code realizing join
+        """
+        inputs = step['inputs']
+        params = [self._result_name(step_id) for step_id in inputs]
+        op_code = f'LogicalJoin({",".join(params)})'
+        return self._assignment(step, op_code)
+    
+    def _LogicalProject(self, step):
+        """ Produce code for projection.
+        
+        Args:
+            step: plan step representing projection
+        
+        Returns:
+            code realizing projection
+        """
+        step_id = step['id']
+        result = self._result_name(step_id)
+        parts = []
+        parts += [f'{result} = Table()']
+        exprs = step['exprs']
+        for expr in exprs:
+            expr_code = self._operation_code(expr)
+            col_code = f'column = {expr_code}'
+            add_code = f'{result}.add_column(column)'
+            parts += [col_code]
+            parts += [add_code]
+        parts += [f'last_result = {result}']
+        return '\n'.join(parts)
+    
     def _LogicalTableScan(self, step):
         """ Produces code for table scan.
         
@@ -43,8 +176,27 @@ class Engine():
         table = step['table'][1]
         file_path = f'{self.data_dir}/{db}/{table}'
         op_code = f'LogicalTableScan({file_path})'
-        result = self._result_name(step)
-        return f'{result} = {op_code}'
+        return self._assignment(step, op_code)
+    
+    def _operation_code(self, operation):
+        """ Generate code realizing given operation. 
+        
+        Args:
+            operation: translate this operation into code
+        
+        Returns:
+            code for realizing operation
+        """
+        if 'literal' in operation:
+            return self._literal_code(operation)
+        elif 'input' in operation:
+            return self._column_code(operation)
+        elif 'op' in operation:
+            syntax = operation['op']['syntax']
+            if syntax == 'BINARY':
+                return self._binary_code(operation)
+        
+        raise ValueError(f'Unhandled operation: {operation}')
     
     def _plan_code(self, plan):
         """ Translates plan into code.
@@ -60,16 +212,16 @@ class Engine():
             lines += [self._step_code(step)]
         return '\n'.join(lines)
     
-    def _result_name(self, step):
+    def _result_name(self, step_id):
         """ Returns name of intermediate result variable.
         
         Args:
-            step: focus on variable storing result of this step
+            step_id: return name of variable storing result of this step
         
         Returns:
             name of variable storing intermediate result of step
         """
-        return f"result_{step['id']}"
+        return f"result_{step_id}"
     
     def _step_code(self, step):
         """ Translates one plan step into code.
