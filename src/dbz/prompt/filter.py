f082238edbcdd6c336683eacc7224d582cbda57b
itrummer <immanuel.trummer@gmail.com>
2022-03-14 17:36:23 -0400

    Changed filter function prompt.

--- src/dbz/prompt/filter.py
+++ src/dbz/prompt/filter.py
@@ -1,12 +1,2 @@
-def pick_rows(table, pick_row):
-    """ Creates new table with subset of rows.
-    
-    <OperatorInstructions>
-    
-    Args:
-        table: select rows from this table
-        pick_row: column with Boolean flags
-    
-    Returns:
-        rows whose flags are set to True
-    """
+def get_rows(table, row_index):
+    """ Returns row where row_index is True. """
