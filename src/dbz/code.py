4422d3dc2a98bb7127123528626621d1b48e9999
itrummer <immanuel.trummer@gmail.com>
2022-03-15 17:16:10 -0400

    Fixed name of equality operator in Calcite plans.

--- src/dbz/code.py
+++ src/dbz/code.py
@@ -80,7 +80,7 @@ class Coder():
                    'LESS_THAN':'less_than',
                    'GREATER_THAN_OR_EQUAL':'greater_than_or_equal',
                    'GREATER_THAN':'greater_than',
-                   'EQUAL_TO':'equal'}[op_kind]
+                   'EQUALS':'equal'}[op_kind]
         operands = operation['operands']
         left_op = self._operation_code(operands[0])
         right_op = self._operation_code(operands[1])
