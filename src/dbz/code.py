fad687cde3d38ad44c0eed6dd9c2ae1726a30149
itrummer <immanuel.trummer@gmail.com>
2022-03-15 17:09:46 -0400

    Updated code generator.

--- src/dbz/code.py
+++ src/dbz/code.py
@@ -236,7 +236,7 @@ class Coder():
         # file_path = f'{self.data_dir}/{db}/{table}'
         table = step['table'][0]
         file_path = f'{self.paths.data_dir}/{table.lower()}.csv'
-        scan_code = f'new_table = Table()\nnew_table.from_csv("{file_path}")'
+        scan_code = f'new_table = Table.from_csv("{file_path}")'
         return scan_code + '\n' + self._assignment(step, 'new_table')
     
     def _operation_code(self, operation):
