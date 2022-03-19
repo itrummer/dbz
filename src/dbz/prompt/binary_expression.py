166db6817f9b451d4fa30167e155c3066b59e4e9
itrummer <immanuel.trummer@gmail.com>
2022-03-19 15:15:19 -0400

    Updated prompt and plan for arithmetic operators.

--- src/dbz/prompt/binary_expression.py
+++ src/dbz/prompt/binary_expression.py
@@ -1,12 +1,10 @@
-def binary_op(operand_1, operand_2):
-    """ Performs binary_op between two operands.
-    
-    <OperatorInstructions>
+def <binary_op>(operand_1, operand_2):
+    """ Performs <binary_op> between two operands.
     
     Args:
-        operand_1: a column or a constant
-        operand_2: a column or a constant
+        operand_1: a column (which <DataInstructions>) or a constant
+        operand_2: a column (which <DataInstructions>) or a constant
     
     Returns:
-        result of binary_op
+        result of <binary_op> (<DataInstructions>)
     """
