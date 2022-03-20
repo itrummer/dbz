8827eaa7629e2932ca6e2c77ebd1e7783ea0c3ac
itrummer <immanuel.trummer@gmail.com>
2022-03-19 22:22:25 -0400

    Added synthesis for row count operation (without distinct).

---
+++ src/dbz/prompt/count.py
@@ -0,0 +1,12 @@
+import os
+
+
+def calculate_row_count(column):
+    """ Calculate row count of values in column.
+    
+    Args:
+        column: the column <DataInstructions>.
+    
+    Returns:
+        row count of column values
+    """
--- src/dbz/prompt/grouped_aggregate.py
+++ src/dbz/prompt/grouped_aggregate.py
@@ -1,17 +1,17 @@
 import os
 
 
-def per_group_<aggregate>(agg_column, group_id_column):
-    """ Calculate <aggregate> for each group.
+def per_group_row_count(column, group_id_column):
+    """ Calculate row count for each group.
     
     1. Collect values for each group.
-    2. Calculate <aggregate> for each group.
-    3. Return dictionary mapping each group ID to the <aggregate>.
+    2. Calculate row count for each group.
+    3. Return dictionary mapping each group ID to the row count.
     
     Args:
-        agg_column: the column <DataInstructions>. It contains values to <aggregate>.
+        column: the column <DataInstructions>. 
         group_id_column: the column <DataInstructions>. It contains for each row the associated group.
     
     Returns:
-        a dictionary mapping each group ID to the <aggregate>
+        a dictionary mapping each group ID to the row count
     """
