0b8f399deb82067f14c47848a5c54cface321528
itrummer <immanuel.trummer@gmail.com>
2022-03-22 13:22:54 -0400

    Added operators for composing Boolean expressions (and, or, not).

---
+++ src/dbz/prompt/boolean_compound.py
@@ -0,0 +1,13 @@
+import os
+
+
+def logical_<operator>(column_1, column_2):
+    """ Performs logical <operator>.
+    
+    Args:
+        column_1: a column (which is a list) with Boolean values
+        column_2: a column (which is a list) with Boolean values
+    
+    Returns:
+        a column (which is a list) containing result of <operator>
+    """
