cb40fc72e8c81d52c3eccba803c1172348b01071
itrummer <immanuel.trummer@gmail.com>
2022-03-22 14:01:54 -0400

    Added prompt for unary operators.

---
+++ src/dbz/prompt/unary.py
@@ -0,0 +1,12 @@
+import os
+
+
+def <operator>(column):
+    """ Performs <operator> on input column.
+    
+    Args:
+        column: a column (which <DataInstructions>) <InputDescription>
+    
+    Returns:
+        a column (which <DataInstructions>) containing result of <operator>
+    """
