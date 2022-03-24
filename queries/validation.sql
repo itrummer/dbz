b81b491b2a3ea6fc56f750dd3e7bfc716d67c2b4
itrummer <immanuel.trummer@gmail.com>
2022-03-24 13:38:43 -0400

    Added Q6 to validation queries.

--- queries/validation.sql
+++ queries/validation.sql
@@ -18,4 +18,5 @@ group by
 	l_linestatus
 order by
 	l_returnflag,
-	l_linestatus
+	l_linestatus;
+select sum(l_extendedprice * l_discount) as revenue from lineitem where l_shipdate >= date '1994-01-01' and l_shipdate < date '1995-1-1' and l_discount between .06 - 0.01 and .06 + 0.01 and l_quantity < 24
