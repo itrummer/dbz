a91dc7033e2bef8f8a4602f854646865b3c1d394
itrummer <immanuel.trummer@gmail.com>
2022-03-24 13:33:59 -0400

    Added missing case when comparing operand scales.

--- src/dbz/code.py
+++ src/dbz/code.py
@@ -389,6 +389,8 @@ class Coder():
             return 0, -scale_2
         elif scale_1 is not None and scale_2 is None:
             return -scale_1, 0
+        elif scale_1 is None and scale_2 is not None:
+            return 0, -scale_2
         elif op_kind in ['LESS_THAN_OR_EQUAL', 'LESS_THAN', 
                          'GREATER_THAN_OR_EQUAL',
                          'GREATER_THAN', 'EQUALS',
