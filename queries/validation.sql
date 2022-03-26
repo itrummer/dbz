d4c7e429b48fde01c1362563b43f08b87fda6cda
itrummer <immanuel.trummer@gmail.com>
2022-03-26 15:36:07 -0400

    Added new TPC-H query for validation (Q5).

--- queries/validation.sql
+++ queries/validation.sql
@@ -41,4 +41,28 @@ group by
 	o_shippriority
 order by
 	revenue desc,
-	o_orderdate
+	o_orderdate;
+select
+	n_name,
+	sum(l_extendedprice * (1 - l_discount)) as revenue
+from
+	customer,
+	orders,
+	lineitem,
+	supplier,
+	nation,
+	region
+where
+	c_custkey = o_custkey
+	and l_orderkey = o_orderkey
+	and l_suppkey = s_suppkey
+	and c_nationkey = s_nationkey
+	and s_nationkey = n_nationkey
+	and n_regionkey = r_regionkey
+	and r_name = 'ASIA'
+	and o_orderdate >= date '1994-01-01'
+	and o_orderdate < date '1994-01-01' + interval '1' year
+group by
+	n_name
+order by
+	revenue desc
