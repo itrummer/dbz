71016aabfbfab3e548393f26df6f560e1b9b178c
itrummer <immanuel.trummer@gmail.com>
2022-03-29 11:47:06 -0400

    Added new TPC-H query to validation set.

--- queries/validation.sql
+++ queries/validation.sql
@@ -110,6 +110,38 @@ order by
 	s_name,
 	p_partkey;
 select
+	c_name,
+	c_custkey,
+	o_orderkey,
+	o_orderdate,
+	o_totalprice,
+	sum(l_quantity)
+from
+	customer,
+	orders,
+	lineitem
+where
+	o_orderkey in (
+		select
+			l_orderkey
+		from
+			lineitem
+		group by
+			l_orderkey having
+				sum(l_quantity) > 300
+	)
+	and c_custkey = o_custkey
+	and o_orderkey = l_orderkey
+group by
+	c_name,
+	c_custkey,
+	o_orderkey,
+	o_orderdate,
+	o_totalprice
+order by
+	o_totalprice desc,
+	o_orderdate;
+select
 	c_custkey,
 	c_name,
 	sum(l_extendedprice * (1 - l_discount)) as revenue,
