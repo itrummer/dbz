2497a25fd4fd1172b84c23d3fd2ea63f7b212520
itrummer <immanuel.trummer@gmail.com>
2022-03-29 17:13:20 -0400

    Testing Q7.

--- queries/test.sql
+++ queries/test.sql
@@ -1,21 +1,39 @@
 select
-	o_orderpriority,
-	count(*) as order_count
+	supp_nation,
+	cust_nation,
+	l_year,
+	sum(volume) as revenue
 from
-	orders
-where
-	o_orderdate >= date '1993-07-01'
-	and o_orderdate < date '1993-07-01' + interval '3' month
-	and exists (
+	(
 		select
-			*
+			n1.n_name as supp_nation,
+			n2.n_name as cust_nation,
+			extract(year from l_shipdate) as l_year,
+			l_extendedprice * (1 - l_discount) as volume
 		from
-			lineitem
+			supplier,
+			lineitem,
+			orders,
+			customer,
+			nation n1,
+			nation n2
 		where
-			l_orderkey = o_orderkey
-			and l_commitdate < l_receiptdate
-	)
+			s_suppkey = l_suppkey
+			and o_orderkey = l_orderkey
+			and c_custkey = o_custkey
+			and s_nationkey = n1.n_nationkey
+			and c_nationkey = n2.n_nationkey
+			and (
+				(n1.n_name = 'FRANCE' and n2.n_name = 'GERMANY')
+				or (n1.n_name = 'GERMANY' and n2.n_name = 'FRANCE')
+			)
+			and l_shipdate between date '1995-01-01' and date '1996-12-31'
+	) as shipping
 group by
-	o_orderpriority
+	supp_nation,
+	cust_nation,
+	l_year
 order by
-	o_orderpriority
+	supp_nation,
+	cust_nation,
+	l_year
