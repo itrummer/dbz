253d8a18ff59866e8aa8dcd0dc1cb8ef98a7174b
itrummer <immanuel.trummer@gmail.com>
2022-03-29 17:12:08 -0400

    Testing Q4

--- queries/test.sql
+++ queries/test.sql
@@ -1,43 +1,21 @@
 select
-	s_acctbal,
-	s_name,
-	n_name,
-	p_partkey,
-	p_mfgr,
-	s_address,
-	s_phone,
-	s_comment
+	o_orderpriority,
+	count(*) as order_count
 from
-	part,
-	supplier,
-	partsupp,
-	nation,
-	region
+	orders
 where
-	p_partkey = ps_partkey
-	and s_suppkey = ps_suppkey
-	and p_size = 15
-	and p_type like '%BRASS'
-	and s_nationkey = n_nationkey
-	and n_regionkey = r_regionkey
-	and r_name = 'EUROPE'
-	and ps_supplycost = (
+	o_orderdate >= date '1993-07-01'
+	and o_orderdate < date '1993-07-01' + interval '3' month
+	and exists (
 		select
-			min(ps_supplycost)
+			*
 		from
-			partsupp,
-			supplier,
-			nation,
-			region
+			lineitem
 		where
-			p_partkey = ps_partkey
-			and s_suppkey = ps_suppkey
-			and s_nationkey = n_nationkey
-			and n_regionkey = r_regionkey
-			and r_name = 'EUROPE'
+			l_orderkey = o_orderkey
+			and l_commitdate < l_receiptdate
 	)
+group by
+	o_orderpriority
 order by
-	s_acctbal desc,
-	n_name,
-	s_name,
-	p_partkey
+	o_orderpriority
