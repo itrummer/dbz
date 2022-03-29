a0e6f5471db2f84ee2553f4f517a5e42c41114e8
itrummer <immanuel.trummer@gmail.com>
2022-03-29 17:14:24 -0400

    Testing Q9.

--- queries/test.sql
+++ queries/test.sql
@@ -1,39 +1,32 @@
 select
-	supp_nation,
-	cust_nation,
-	l_year,
-	sum(volume) as revenue
+	nation,
+	o_year,
+	sum(amount) as sum_profit
 from
 	(
 		select
-			n1.n_name as supp_nation,
-			n2.n_name as cust_nation,
-			extract(year from l_shipdate) as l_year,
-			l_extendedprice * (1 - l_discount) as volume
+			n_name as nation,
+			extract(year from o_orderdate) as o_year,
+			l_extendedprice * (1 - l_discount) - ps_supplycost * l_quantity as amount
 		from
+			part,
 			supplier,
 			lineitem,
+			partsupp,
 			orders,
-			customer,
-			nation n1,
-			nation n2
+			nation
 		where
 			s_suppkey = l_suppkey
+			and ps_suppkey = l_suppkey
+			and ps_partkey = l_partkey
+			and p_partkey = l_partkey
 			and o_orderkey = l_orderkey
-			and c_custkey = o_custkey
-			and s_nationkey = n1.n_nationkey
-			and c_nationkey = n2.n_nationkey
-			and (
-				(n1.n_name = 'FRANCE' and n2.n_name = 'GERMANY')
-				or (n1.n_name = 'GERMANY' and n2.n_name = 'FRANCE')
-			)
-			and l_shipdate between date '1995-01-01' and date '1996-12-31'
-	) as shipping
+			and s_nationkey = n_nationkey
+			and p_name like '%green%'
+	) as profit
 group by
-	supp_nation,
-	cust_nation,
-	l_year
+	nation,
+	o_year
 order by
-	supp_nation,
-	cust_nation,
-	l_year
+	nation,
+	o_year desc
