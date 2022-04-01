b81168b64cf172d8f9037cca2d4045ef88f131ce
itrummer <immanuel.trummer@gmail.com>
2022-03-31 22:07:53 -0400

    Added missing prompt file for calculating number of rows in column.

---
+++ src/dbz/prompt/nr_rows.py
@@ -0,0 +1,12 @@
+import os
+
+
+def nr_rows(column):
+    """ Returns number of rows in a column (which <DataInstructions>).
+    
+    Args:
+        column: a column (which <DataInstructions>)
+    
+    Returns:
+        number of elements in column
+    """
