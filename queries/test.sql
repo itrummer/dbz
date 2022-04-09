6df8b3288f94e5a9275076b6cc1a08d781a25414
itrummer <immanuel.trummer@gmail.com>
2022-04-08 22:31:01 -0400

    Added more parts of Q22 to test query.

--- queries/test.sql
+++ queries/test.sql
@@ -16,3 +16,11 @@ select
 					and substring(c_phone from 1 for 2) in
 						('13', '31', '23', '29', '30', '18', '17')
 			)
+			and not exists (
+				select
+					*
+				from
+					orders
+				where
+					o_custkey = c_custkey
+			)
