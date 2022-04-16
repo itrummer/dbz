9836103ad00d5fbacc88afe2a7ba7cbbc47c66bf
itrummer <immanuel.trummer@gmail.com>
2022-04-16 16:13:57 -0400

    Added prompt for item access function.

---
+++ src/dbz/prompt/get.py
@@ -0,0 +1,10 @@
+def get_value(column, index):
+    """ Returns value in column at given index.
+    
+    Args:
+        column: a column (which <DataInstructions>)
+        index: item index
+    
+    Returns:
+        column value at given index
+    """
