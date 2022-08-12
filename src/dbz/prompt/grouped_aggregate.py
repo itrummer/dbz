ac10fb539e417a64f1d34531df548a4c7802e072
itrummer <immanuel.trummer@gmail.com>
2022-08-11 22:27:56 -0400

    Fixed prompt for grouped aggregation.

--- src/dbz/prompt/grouped_aggregate.py
+++ src/dbz/prompt/grouped_aggregate.py
@@ -1,11 +1,11 @@
 def group_by_<aggregate>(table, agg_column, group_columns):
-    """ Calculate <aggregate> for each group.
+    """ Calculate <aggregate> for each value combination in group columns.
    
     Args:
         table: <Table>.
         agg_column: index of column for which to calculate <aggregate>.
-        group_by_cols: indexes of columns to group by.
+        group_columns: indexes of columns to group by.
    
     Returns:
-        <Table> with group columns and one <aggregate> column.
+        group columns and associated <aggregate>.
     """
