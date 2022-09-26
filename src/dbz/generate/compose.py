6583984dcce7817fc73545d881a815b0d1d1d18e
itrummer <immanuel.trummer@gmail.com>
2022-09-26 18:49:42 -0400

    Added first version of composer (untested), simplified op manager.

--- src/dbz/generate/compose.py
+++ src/dbz/generate/compose.py
@@ -3,3 +3,161 @@ Created on Sep 25, 2022
 
 @author: immanueltrummer
 '''
+from collections import defaultdict
+import dbz.execute.engine
+
+
+class Composer():
+    """ Composes operator implementations into full SQL engine. """
+    
+    def __init__(self, config, operators, tasks):
+        """ Initialize with operator manager. 
+        
+        Args:
+            config: JSON file configuring synthesis
+            operators: manages operator implementations
+            tasks: manages generation tasks and checks
+        """
+        self.ops = operators
+        self.tasks = tasks
+        self.gen_tasks = tasks.gen_tasks
+        self.task_order = [t['task_id'] for t in self.gen_tasks]
+        self.nr_tasks = len(self.task_order)
+        self.idx2checks = self._schedule_checks()
+        self.compositions = defaultdict(lambda:[])
+        
+        ref_access = config['ref_access']
+        pg_db = ref_access['pg_db']
+        pg_user = ref_access['pg_user']
+        pg_pwd = ref_access['pg_pwd']
+        pg_host = ref_access['host']
+        self.ref_engine = dbz.execute.engine.PgEngine(
+            pg_db, pg_user, pg_pwd, pg_host)
+        
+        test_access = self.config['test_access']
+        data_dir = test_access['data_dir']
+        self.paths = dbz.util.DbzPaths(data_dir)
+        self.python_path = test_access['python']
+    
+    def finished(self):
+        """ Checks whether a complete engine was generated. 
+        
+        Returns:
+            True if a working SQL engine was generated
+        """
+        return self.compositions[self.nr_tasks-1]
+    
+    def update(self, updated_task, new_code_id):
+        """ Update compositions using new implementation. 
+        
+        Args:
+            updated_task: new implementation for this task
+            new_code_id: ID of new code for updated task
+        """
+        updated_idx = self.task_order.index(updated_task)
+        last_result = self.compositions[updated_idx-1]
+        
+        for task_idx in range(updated_idx, self.nr_tasks):
+            updated = (task_idx == updated_idx)
+            task = self.task_order[task_idx]
+            cur_code_ids = [new_code_id] if updated else self.ops.get(task)
+            
+            next_result = []
+            for last_row in last_result:
+                for cur_code_id in cur_code_ids:
+                    next_row = last_row.copy()
+                    next_row[task] = cur_code_id
+                    next_result += [next_row]
+        
+            last_result = self._filter(next_result, task_idx)
+            self.compositions[task_idx] += last_result
+    
+    def _applicable_checks(self, tasks):
+        """ Retrieves applicable checks, given finished tasks.
+        
+        Args:
+            tasks: list of finished tasks
+        
+        Returns:
+            set of applicable check IDs
+        """
+        tasks_set = set(tasks)
+        applicable = set()
+        for check_idx, check in enumerate(self.checks):
+            required_set = set(check['requirements'])
+            if tasks_set.issubset(required_set):
+                applicable.add(check_idx)
+        return applicable
+    
+    def _check(self, composition, check):
+        """ Apply check to given composition. 
+        
+        Args:
+            composition: maps tasks to selected implementations
+            check: a test case for the given composition
+        
+        Returns:
+            True iff the composition passes the check
+        """
+        library = self._composition_code(composition)
+        queries = [check['query']]
+        test_engine = dbz.execute.engine.DbzEngine(
+            self.paths, library, self.python)
+        validator = dbz.execute.check.Validator(
+            self.paths, queries, self.ref_engine)
+        return validator.validate(test_engine)
+    
+    def _composition_code(self, composition):
+        """ Translates composition into a piece of code.
+        
+        Args:
+            composition: maps tasks to code IDs
+        
+        Returns:
+            a piece of Python code
+        """
+        parts = []
+        for task, code_id in composition.items():
+            code = self.ops.get_ops(task)[code_id]
+            parts += [code]
+        return '\n\n'.join(parts)
+    
+    def _filter(self, result, last_task_idx):
+        """ Filter result tuples via newly applicable predicates.
+        
+        Args:
+            result: list containing result tuples
+            last_task_idx: last treated task index
+        
+        Returns:
+            list of tuples satisfying all predicates
+        """
+        checks = self.idx2checks[last_task_idx]
+        filtered = []
+        for row in result:
+            success = True
+            for check in checks:
+                if not self._check(row, check):
+                    success = False
+                    break
+            
+            if success:
+                filtered += [row]
+        return filtered
+        
+    def _schedule_checks(self):
+        """ Schedule checks based on task order. 
+        
+        Returns:
+            dictionary mapping task indexes to list of checks
+        """
+        idx2checks = {}
+        for task_idx in range(self.nr_tasks):
+            prior_tasks = [self.task_order[i] for i in range(task_idx)]
+            next_tasks = prior_tasks + [task_idx]
+            prior_check_ids = self._applicable_checks(prior_tasks)
+            next_check_ids = self._applicable_checks(next_tasks)
+            new_check_ids = next_check_ids - prior_check_ids
+            new_checks = [self.tasks.check_tasks[i] for i in new_check_ids]
+            idx2checks[task_idx] = new_checks
+        return idx2checks
--- src/dbz/generate/operator.py
+++ src/dbz/generate/operator.py
@@ -10,8 +10,8 @@ class Operators():
     
     def __init__(self):
         """ Initializes operator implementations. """
-        self.ops_by_task = defaultdict(lambda:set())
-        self.code_temp = defaultdict(lambda:1)
+        self.ops_by_task = defaultdict(lambda:[])
+        self.code_temp = {}
     
     def add_op(self, task_id, code, temperature):
         """ Adds operator implementation.
@@ -24,13 +24,14 @@ class Operators():
         Returns:
             True if code was added (i.e., formerly unknown)
         """
-        prior_temp = self.code_temp[code]
-        self.code_temp[code] = min(prior_temp, temperature)
-        ops = self.ops_by_task[task_id]
-        if code in ops:
+        if code in self.code_temp:
+            prior_temp = self.code_temp[code]
+            next_temp = min(prior_temp, temperature)
+            self.code_temp[code] = next_temp
             return False
         else:
-            ops.add(code)
+            self.code_temp[code] = temperature
+            self.ops_by_task[task_id] += [code]
             return True
     
     def get_ops(self, task_id):
@@ -40,12 +41,10 @@ class Operators():
             task_id: the ID of the generation task
         
         Returns:
-            list of (code,temperature) pairs, sorted by temperature (ascending)
+            list of (code,temperature) pairs
         """
-        ops = list(self.ops_by_task[task_id])
-        ops_temp = [(op, self.code_temp[op]) for op in ops]
-        ops_temp.sort(key=lambda o_t:o_t[1])
-        return ops_temp
+        ops = self.ops_by_task[task_id]
+        return [(op, self.code_temp[op]) for op in ops]
 
 
 if __name__ == '__main__':
