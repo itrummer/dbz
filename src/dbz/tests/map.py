7b40d975bda24bfec68405d6840612c3ff61ef36
itrummer <immanuel.trummer@gmail.com>
2022-10-05 00:25:49 -0400

    Corrected name of mapping function in code test case.

--- src/dbz/tests/map.py
+++ src/dbz/tests/map.py
@@ -4,7 +4,7 @@ Created on Oct 4, 2022
 @author: immanueltrummer
 '''
 c = fill_column(1, 5)
-map(c, lambda x:x+1)
+map_column(c, lambda x:x+1)
 c2 = fill_column(2, 5)
-map(c2, lambda x:x+10)
+map_column(c2, lambda x:x+10)
 last_result = create_table([c, c2])
