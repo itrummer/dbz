52b33f89a73d3c6473e724846531968bc418a673
itrummer <immanuel.trummer@gmail.com>
2022-10-06 14:35:17 -0400

    Updated substring function.

--- src/dbz/prompt/substring.py
+++ src/dbz/prompt/substring.py
@@ -1,11 +1,14 @@
 def substring(column, start, length):
-    """ Extracts substring from each column row.
+    """ Extracts substring from each column row:
+    
+    1. Maps <Null> to <Null>.
+    2. Maps strings to substrings.
     
     Args:
-        column: <Column> containing strings.
+        column: <Column> containing strings or <Null>.
         start: start index of substring (count starts with 1).
         length: length of substring.
     
     Returns:
-        <Column> containing substrings
+        <Column> containing substrings or <Null>.
     """
