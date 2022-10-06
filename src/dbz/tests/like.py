02704ccaf09cb47cbc4291f7a25e4a1c0e8ab1e2
itrummer <immanuel.trummer@gmail.com>
2022-10-05 23:25:55 -0400

    Fixed reference to LIKE operator in test case.

--- src/dbz/tests/like.py
+++ src/dbz/tests/like.py
@@ -5,7 +5,7 @@ Created on Oct 5, 2022
 '''
 c0 = fill_column(get_null(), 10)
 c1 = fill_column('asdfaswtv zaaq2', 10)
-c2 = like('%fas_', c0)
-c3 = like('%fas_', c1)
-c4 = like('%asfdkhjqwetrkj', c1)
+c2 = like_expression('%fas_', c0)
+c3 = like_expression('%fas_', c1)
+c4 = like_expression('%asfdkhjqwetrkj', c1)
 last_result = create_table([c0, c1, c2, c3, c4])
