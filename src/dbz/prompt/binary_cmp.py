b205d45567857e63564bf993aedae644ccb94dda
itrummer <immanuel.trummer@gmail.com>
2022-10-20 22:50:27 -0400

    Clarified prompt for binary comparison operations.

--- src/dbz/prompt/binary_cmp.py
+++ src/dbz/prompt/binary_cmp.py
@@ -1,10 +1,14 @@
 def <binary_op>(column_1, column_2):
-    """ True where operand_1 <short_op> operand_2.
+    """ True where column_1 <short_op> column_2.
+    
+    1. Return <Null> for rows where one input row is <Null>.
+    2. Return true iff first row <short_op> second row otherwise.
+    3. Ensure that the output is <Column>.
     
     Args:
         column_1: <Column>.
         column_2: <Column>.
     
     Returns:
-        <Column> containing Boolean values, <Null> where any of the operands is <Null>.
+        <Column> containing Boolean values.
     """
