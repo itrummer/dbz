e384a530cc70b2e3292878f14a9147b31039376b
itrummer <immanuel.trummer@gmail.com>
2022-10-20 23:18:07 -0400

    Refined prompt for table generation.

--- src/dbz/prompt/create_table.py
+++ src/dbz/prompt/create_table.py
@@ -1,8 +1,11 @@
 def create_table(column_data):
     """ Create table from given columns.
+    
+    1. Insert column_data as columns into <Table>.
+    2.<TablePost>
    
     Args:
-        column_data: a list of columns where each column is <Column>.<TablePost>
+        column_data: a list of columns where each column is <Column>.
    
     Returns:
         <Table>.
