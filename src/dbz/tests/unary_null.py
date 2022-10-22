66943171c6d27f17a465495567162d2928b9733b
itrummer <immanuel.trummer@gmail.com>
2022-10-22 16:42:42 -0400

    Fixed test with null values for unary operators.

--- src/dbz/tests/unary_null.py
+++ src/dbz/tests/unary_null.py
@@ -10,8 +10,8 @@ for c in [c1, c2]:
     r = <SubstituteBy:cast_to_int|cast_to_float|cast_to_string>(c)
     results += [r]
 
-check_null = is_null(r[0])
-check_not_null = is_null(r[1])
+check_null = is_null(results[0])
+check_not_null = is_null(results[1])
 for i in range(10):
     assert get_value(check_null, i)
     assert not get_value(check_not_null, i)
