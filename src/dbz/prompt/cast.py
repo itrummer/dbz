6f0b65dbc5b8b004394ea709863323151f3f18c4
itrummer <immanuel.trummer@gmail.com>
2022-10-23 12:27:16 -0400

    Refined casting prompt to ensure correct output format.

--- src/dbz/prompt/cast.py
+++ src/dbz/prompt/cast.py
@@ -1,9 +1,12 @@
 def cast_to_<ToType>(column):
     """ Casts a column to <ToType> values.
     
+    1. Transform input column into <ToType> values.
+    2. Ensure output is <Column>.
+    
     Args:
         column: <Column>.
     
     Returns:
-        <Column> with <ToType> values.
+        <Column>.
     """
