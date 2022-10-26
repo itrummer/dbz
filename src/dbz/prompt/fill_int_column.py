02110138c24a458d6f57c2abfa84034e39acd635
itrummer <immanuel.trummer@gmail.com>
2022-10-25 20:41:57 -0400

    Added instructions on NULL constants in fill_integer_column prompt.

--- src/dbz/prompt/fill_int_column.py
+++ src/dbz/prompt/fill_int_column.py
@@ -1,7 +1,7 @@
 def fill_int_column(constant, nr_rows):
     """ Returns a column, filled with <IntegerField> values.
     
-    1. Round input constant to integer value.
+    1. If constant is not <Null>, round it to integer value.
     2. Create column filled with int values.
     3. Ensure that column type is <IntegerField>.
     
