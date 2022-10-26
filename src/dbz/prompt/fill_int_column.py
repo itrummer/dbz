c114275547548ffad8c0e4035f71194fd6078c37
itrummer <immanuel.trummer@gmail.com>
2022-10-25 20:24:28 -0400

    Added instructions for rounding in fill integer column prompt.

--- src/dbz/prompt/fill_int_column.py
+++ src/dbz/prompt/fill_int_column.py
@@ -1,11 +1,12 @@
 def fill_int_column(constant, nr_rows):
     """ Returns a column, filled with <IntegerField> values.
     
-    1. Create column filled with int values.
-    2. Ensure that column type is <IntegerField>.
+    1. Round input constant to integer value.
+    2. Create column filled with int values.
+    3. Ensure that column type is <IntegerField>.
     
     Args:
-        constant: int value or <Null>.
+        constant: int or float value or <Null>.
         nr_rows: number of rows in result column.
     
     Returns:
