04f41b9371dc5d9f49d395a41e572064b3383f7c
itrummer <immanuel.trummer@gmail.com>
2022-10-27 19:33:11 -0400

    Added missing prompt for rounding operator.

---
+++ src/dbz/prompt/round.py
@@ -0,0 +1,11 @@
+def round(column):
+    """ Rounds column values.
+    
+    1. Map <Null> to <Null>.
+    2. Round other values.
+    
+    Args:
+        column: <Column>.
+    Returns:
+        <Column> with rounded values.
+    """
