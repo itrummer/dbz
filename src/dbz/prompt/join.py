762b790e4e9c51687f262868d9d1afa580b1f377
itrummer <immanuel.trummer@gmail.com>
2022-10-28 18:05:29 -0400

    Refined join prompt to encourage indexing by join columns.

--- src/dbz/prompt/join.py
+++ src/dbz/prompt/join.py
@@ -1,15 +1,18 @@
-def <JoinDefinition>_join(table_1, table_2, key_cols_1, key_cols_2):
-    """ Join two tables by <JoinDescription> join.
-
-    1. Perform <JoinDescription> join.
-    2.<TablePost>
+def equality_join(table_1, table_2, key_cols_1, key_cols_2):
+    """ Join two tables by equality join. 
+    
+    1. Index table_1 on key_cols_1.
+    2. Index table_2 on key_cols_2.
+    3. Join table_1 and table_2 with equality join.
+    4.<TablePost>
 
     Args:
         table_1: <Table>.
         table_2: <Table>.
         key_cols_1: indexes of join key columns in table 1.
         key_cols_2: indexes of join key columns in table 2.
-   
+
     Returns:
         Joined rows with matching join keys.
     """
+    def index(
