c1ef342665a8ec544eadc62331c8a84e5390992d
itrummer <immanuel.trummer@gmail.com>
2022-10-28 18:11:29 -0400

    Re-introduced placeholder for join type in join prompt.

--- src/dbz/prompt/join.py
+++ src/dbz/prompt/join.py
@@ -1,9 +1,9 @@
-def equality_join(table_1, table_2, key_cols_1, key_cols_2):
-    """ Join two tables by equality join. 
+def <JoinDefinition>_join(table_1, table_2, key_cols_1, key_cols_2):
+    """ Join two tables by <JoinDefinition> join. 
     
     1. Index table_1 on key_cols_1.
     2. Index table_2 on key_cols_2.
-    3. Join table_1 and table_2 with equality join.
+    3. Join table_1 and table_2 with <JoinDefinition> join.
     4.<TablePost>
 
     Args:
