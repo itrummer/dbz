6a711836a354b0fa4826758b717cc32a836a2679
itrummer <immanuel.trummer@gmail.com>
2022-10-30 16:53:39 -0400

    Added specialized test cases for outer joins variants.

---
+++ src/dbz/tests/left_join.py
@@ -0,0 +1,17 @@
+'''
+Created on Oct 30, 2022
+
+@author: immanueltrummer
+'''
+c1 = fill_int_column(1,10)
+c2 = fill_int_column(0,10)
+t1 = create_table([c1, c2])
+t2 = create_table([c1, c2])
+r = left_outer_join(t1, t2, [1], [1])
+assert table_cardinality(r) == 100
+r = left_outer_join(t1, t2, [0], [0])
+assert table_cardinality(r) == 100
+r = left_outer_join(t1, t2, [1], [0])
+assert table_cardinality(r) == 100
+r = left_outer_join(t1, t2, [0], [1])
+assert table_cardinality(r) == 100
