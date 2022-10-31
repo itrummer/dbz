8ebb319f827310172ccb1eeadc8ce32c32fa51c1
itrummer <immanuel.trummer@gmail.com>
2022-10-31 11:39:47 -0400

    Clarified rounding method in prompt for column rounding function.

--- src/dbz/prompt/round.py
+++ src/dbz/prompt/round.py
@@ -2,7 +2,9 @@ def round_column(column):
     """ Rounds column values.
     
     1. Map <Null> to <Null>.
-    2. Round other values.
+    2. Round other values:
+    - Add 0.5
+    - Use floor
     
     Args:
         column: <Column>.
