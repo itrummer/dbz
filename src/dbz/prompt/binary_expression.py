2801108953b7e67f64a17cf1a4442a74466ec8bc
itrummer <immanuel.trummer@gmail.com>
2022-11-01 19:15:30 -0400

    Explicitly refer to element-wise operation in binary op prompt.

--- src/dbz/prompt/binary_expression.py
+++ src/dbz/prompt/binary_expression.py
@@ -1,10 +1,13 @@
 def <binary_op>(column_1, column_2):
     """ Performs <binary_op> between two columns.
     
+    1. Rows with <Null> in column_1 or column_2 map to <Null>.
+    2. Element-wise <binary_op> of column_1 and column_2.
+
     Args:
         operand_1: <Column>.
         operand_2: <Column>.
-    
+
     Returns:
-        result column (which is <Column>), <Null> where any of the operands is <Null>
+        <Column>.
     """
