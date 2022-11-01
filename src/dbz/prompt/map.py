805d332caca127a3cc5ec1f0a55eb369ce533de3
itrummer <immanuel.trummer@gmail.com>
2022-11-01 14:04:09 -0400

    Added instructions on NULL handling in column mapping prompt.

--- src/dbz/prompt/map.py
+++ src/dbz/prompt/map.py
@@ -1,10 +1,13 @@
 def map_column(column, map_fct):
     """ Applies function to each element of the column.
     
+    1. Maps <Null> to <Null>.
+    2. Use map_fct on other values.
+    
     Args:
         column: <Column>.
         map_fct: apply to each element in column.
     
     Returns:
-        <Column> with the result of function (<Null> if input is <Null>).
+        <Column>.
     """
