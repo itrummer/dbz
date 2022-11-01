c2082122681af08a15c74a4b71c7ff48e86fd919
itrummer <immanuel.trummer@gmail.com>
2022-11-01 18:20:41 -0400

    Refined prompt for sort operator.

--- src/dbz/prompt/sort.py
+++ src/dbz/prompt/sort.py
@@ -1,10 +1,13 @@
 def sort_rows(table, key_columns):
-    """ Sort rows by key columns.<TablePost>
+    """ Sort rows by key columns. 
     
+    1. Sort rows by key columns.
+    2.<TablePost>
+
     Args:
         table: <Table>.
         key_columns: list of column indexes.
-    
+
     Returns:
-        <Table> (without index) containing rows sorted by key columns.
+        <Table> (sorted).
     """
