6ae634bee5105cb5291d46a7c89036a602c33fdf
itrummer <immanuel.trummer@gmail.com>
2022-11-01 22:43:18 -0400

    Described handling of NULL values in updated sort prompt.

--- src/dbz/prompt/sort.py
+++ src/dbz/prompt/sort.py
@@ -1,7 +1,9 @@
 def sort_rows(table, key_columns):
     """ Sort rows by key columns. 
     
-    1. Sort rows by key columns.
+    1. Sort rows by key columns using following comparator:
+    - Compare values in key_columns order.
+    - <Null> values are sorted last.
     2.<TablePost>
 
     Args:
