ba2b39cfff852afc6a84f9c737dd4476c06864e8
itrummer <immanuel.trummer@gmail.com>
2022-11-02 23:31:54 -0400

    Fixed post-processing code.

--- src/dbz/execute/code.py
+++ src/dbz/execute/code.py
@@ -615,9 +615,7 @@ class Coder():
                 if 'scale' in col_type:
                     scale = col_type['scale']
                     parts += [f'col = multiply_by_scalar(col, 1e-{scale})']
-                if 'precision' in col_type:
-                    precision = col_type['precision']
-                    parts += [f'col = map_column(lambda i:round(i,{precision}))']
+                    parts += [f'col = map_column(col, lambda i:round(i,{scale}))']
             elif base_type in ['CHAR']:
                 length = col_type['precision']
                 parts += [f'col = smart_padding(col, {length})']
