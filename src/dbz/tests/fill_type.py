51375783c1761004846d23897cd1f2f32fb40661
itrummer <immanuel.trummer@gmail.com>
2022-11-06 17:39:45 -0500

    Corrected test of column type for filling functions.

--- src/dbz/tests/fill_type.py
+++ src/dbz/tests/fill_type.py
@@ -4,5 +4,5 @@ Created on Nov 6, 2022
 @author: immanueltrummer
 '''
 n = get_null()
-nc = fill_<SubstituteBy:Boolean|int|float|string>_column
+nc = fill_<SubstituteBy:Boolean|int|float|string>_column(n, 10)
 assert check_column_type(nc)
