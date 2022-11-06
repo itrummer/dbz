3adceb84b9820502a613a6996e533f4c7b36f6c4
itrummer <immanuel.trummer@gmail.com>
2022-11-06 17:56:55 -0500

    Fixed test of Boolean column fill operator.

--- src/dbz/tests/fill_Boolean.py
+++ src/dbz/tests/fill_Boolean.py
@@ -3,8 +3,8 @@ Created on Nov 6, 2022
 
 @author: immanueltrummer
 '''
-tc = fill_Boolean_column(False, 10)
-fc = fill_Boolean_column(True, 10)
+tc = fill_Boolean_column(True, 10)
+fc = fill_Boolean_column(False, 10)
 for i in range(10):
     assert get_value(tc, i)
     assert not get_value(fc, i)
