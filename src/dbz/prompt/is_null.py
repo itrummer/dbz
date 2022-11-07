1107835ef8ec173960afb80d8a7ad55afd9273a5
itrummer <immanuel.trummer@gmail.com>
2022-11-06 19:10:14 -0500

    Made prompt for null check more detailed.

--- src/dbz/prompt/is_null.py
+++ src/dbz/prompt/is_null.py
@@ -1,6 +1,9 @@
 def is_null(column):
     """ Checks if values in a column are <Null>.
     
+    1. <Null> maps to True.
+    2. Other values map to False.
+    
     Args:
         column: <Column>.
     
