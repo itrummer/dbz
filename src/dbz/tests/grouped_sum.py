e3e85afa07ef46eb40e6609da5b5c4fd7c50ed57
itrummer <immanuel.trummer@gmail.com>
2022-11-06 23:47:20 -0500

    Added test cases for grouped aggregation.

---
+++ src/dbz/tests/grouped_sum.py
@@ -0,0 +1,13 @@
+'''
+Created on Nov 6, 2022
+
+@author: immanueltrummer
+'''
+c = fill_int_column(1, 10)
+t = create_table(c, c)
+a = group_by_sum(t, [0], [1])
+card = table_cardinality(a) 
+assert card == 1, card
+r = get_column(a, 1)
+v = get_value(r, 0)
+assert v == 10, v
