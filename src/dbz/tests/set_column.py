c3968b4bf36cbf294f46875b7db149f7144596f7
itrummer <immanuel.trummer@gmail.com>
2022-11-08 18:27:37 -0500

    Relaxed assumption that set_column does not change input table.

--- src/dbz/tests/set_column.py
+++ src/dbz/tests/set_column.py
@@ -8,7 +8,7 @@ c2 = fill_int_column(2, 10)
 t1 = create_table([c1, c1])
 t2 = set_column(t1, 0, c2)
 
-assert get_value(get_column(t1, 0), 0) == 1
-assert get_value(get_column(t1, 1), 0) == 1
+#assert get_value(get_column(t1, 0), 0) == 1
+#assert get_value(get_column(t1, 1), 0) == 1
 assert get_value(get_column(t2, 0), 0) == 2
 assert get_value(get_column(t2, 1), 0) == 1
