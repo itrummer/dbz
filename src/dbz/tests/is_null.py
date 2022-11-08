79baca9210762c63f20594712bcad752ad5ea92a
itrummer <immanuel.trummer@gmail.com>
2022-11-08 00:46:15 -0500

    Refined test cases for data loading and filling columns.

--- src/dbz/tests/is_null.py
+++ src/dbz/tests/is_null.py
@@ -3,9 +3,24 @@ Created on Nov 6, 2022
 
 @author: immanueltrummer
 '''
+def run_checks(c, nc):
+    assert not get_value(is_null(c), 0)
+    assert get_value(is_null(nc), 0)
+    assert check_column_type(is_null(c))
+    assert check_column_type(is_null(nc))
+
+c = fill_Boolean_column(True, 10)
+nc = fill_Boolean_column(get_null(), 10)
+run_checks(c, nc)
+
 c = fill_int_column(1, 10)
 nc = fill_int_column(get_null(), 10)
-assert not get_value(is_null(c), 0)
-assert get_value(is_null(nc), 0)
-assert check_column_type(is_null(c))
-assert check_column_type(is_null(nc))
+run_checks(c, nc)
+
+c = fill_float_column(1.5, 10)
+nc = fill_float_column(get_null(), 10)
+run_checks(c, nc)
+
+c = fill_string_column('this is a test', 10)
+nc = fill_string_column(get_null(), 10)
+run_checks(c, nc)
--- src/dbz/tests/load.py
+++ src/dbz/tests/load.py
@@ -5,6 +5,7 @@ Created on Nov 6, 2022
 '''
 t = load_from_csv('test_data/test1.csv')
 assert check_table_type(t)
+assert table_cardinality(t) == 3
 
 c = get_column(t, 3)
 n = is_null(c)
