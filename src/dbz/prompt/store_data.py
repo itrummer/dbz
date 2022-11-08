dcc71c48633df06b4effee3351c3da06c8ab5643
itrummer <immanuel.trummer@gmail.com>
2022-11-08 18:51:08 -0500

    Specified output format in more detail.

--- src/dbz/prompt/store_data.py
+++ src/dbz/prompt/store_data.py
@@ -2,7 +2,7 @@ def write_to_csv(table, path_to_csv):
     """ Writes table to a .csv file (without header).
     
     1. Make necessary transformations, if any.
-    2. Use pandas (pd) to_csv method to write to file.
+    2. Use pandas (pd) to_csv method to write to file (no index, no header).
     
     Args:
         table: <Table>.
