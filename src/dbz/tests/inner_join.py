05ce4170c1d7c5acb8ff82d851e19ab1dfc353cf
itrummer <immanuel.trummer@gmail.com>
2022-11-07 23:46:37 -0500

    Added test cases for setting columns and for inner join.

--- src/dbz/tests/inner_join.py
+++ src/dbz/tests/inner_join.py
@@ -7,11 +7,29 @@ c1 = fill_int_column(1,10)
 c2 = fill_int_column(0,10)
 t1 = create_table([c1, c2])
 t2 = create_table([c1, c2])
+
+def get_row(table):
+    vals = []
+    for col_idx in range(4):
+        col = get_column(table, col_idx)
+        val = get_value(col, 0)
+        vals += [val]
+    return vals
+
 r = equality_join(t1, t2, [1], [1])
 assert table_cardinality(r) == 100
+assert get_row(table) == [1, 0, 1, 0]
+
 r = equality_join(t1, t2, [0], [0])
 assert table_cardinality(r) == 100
+assert get_row(table) == [1, 0, 1, 0]
+
 r = equality_join(t1, t2, [1], [0])
 assert table_cardinality(r) == 0
+
 r = equality_join(t1, t2, [0], [1])
 assert table_cardinality(r) == 0
+
+r = equality_join(t1, t2, [1], [1])
+r = equality_join(r, t2, [1], [1])
+assert table_cardinality(r) == 1000
---
+++ src/dbz/tests/set_column.py
@@ -0,0 +1,14 @@
+'''
+Created on Nov 7, 2022
+
+@author: immanueltrummer
+'''
+c1 = fill_int_column(1, 10)
+c2 = fill_int_column(2, 10)
+t1 = create_table([c1, c1])
+t2 = set_column(t1, 0, c2)
+
+assert get_value(get_column(t1, 0), 0) == 1
+assert get_value(get_column(t1, 1), 0) == 1
+assert get_value(get_column(t2, 0), 0) == 2
+assert get_value(get_column(t2, 1), 0) == 1
