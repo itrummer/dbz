7de8b4dd9912b834ae10e9053d85ecc6a280e7d2
itrummer <immanuel.trummer@gmail.com>
2022-11-08 18:33:39 -0500

    Refined test case by checking table type after table creation.

--- src/dbz/tests/create_table.py
+++ src/dbz/tests/create_table.py
@@ -5,7 +5,9 @@ Created on Nov 8, 2022
 '''
 t = create_table([])
 assert is_empty(t)
+assert check_table_type(t)
 
 c = fill_int_column(1, 10)
 t2 = create_table([c, c])
 assert not is_empty(t2)
+assert check_table_type(t2)
