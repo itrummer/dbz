4876e1f3e8a7cd3b2b947b53f01cb41bda6cc673
itrummer <immanuel.trummer@gmail.com>
2022-11-09 06:17:00 -0500

    Refined test case for store operator using reference output.

--- src/dbz/tests/store.py
+++ src/dbz/tests/store.py
@@ -3,6 +3,9 @@ Created on Nov 8, 2022
 
 @author: immanueltrummer
 '''
+import filecmp
+
 c = fill_int_column(1, 10)
 t = create_table([c, c])
 write_to_csv(t, 'dummy.csv')
+assert filecmp.cmp('dummy.csv', 'test_output/store.csv')
