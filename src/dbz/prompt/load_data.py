3d2c84fbf5e4fbf7ce0058db2a1c64a453f2c35f
itrummer <immanuel.trummer@gmail.com>
2022-11-08 19:14:01 -0500

    Refined load prompt.

--- src/dbz/prompt/load_data.py
+++ src/dbz/prompt/load_data.py
@@ -1,7 +1,7 @@
 def load_from_csv(path_to_csv):
     """ Load data from .csv file (without header).
     
-    1. Use pandas (pd) to load .csv file from disk.
+    1. Use pandas (pd) to load .csv file from disk (no header).
     2. Transform into <Table>.
     3. Replace nan values by <Null>.
     4.<TablePost>
