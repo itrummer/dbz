001fb0d5d7beb05113fed824e3104cd50878bace
itrummer <immanuel.trummer@gmail.com>
2022-11-09 19:10:58 -0500

    Described cross product in more detail.

--- src/dbz/prompt/cross_product.py
+++ src/dbz/prompt/cross_product.py
@@ -1,8 +1,11 @@
 def cross_product(table_1, table_2):
     """ Performs a cross product between tables.
 
-    1. Create new table: cross product between table 1 and table 2.
-    2.<TablePost>
+    1. Iterate over all rows from table_1:
+      - Iterate over all rows from table_2:
+        - Concatenate rows and add to result.
+    2. Transform into <Table>.
+    3.<TablePost>
 
     Args:
         table_1: <Table>.
