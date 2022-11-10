0d7f04aa20aca04b1aaaf629a220bf88a7bb5052
itrummer <immanuel.trummer@gmail.com>
2022-11-10 14:59:16 -0500

    Fixed reference to write_to_csv in grouped aggregation test case.

--- src/dbz/tests/grouped_sum.py
+++ src/dbz/tests/grouped_sum.py
@@ -6,5 +6,5 @@ Created on Nov 6, 2022
 c = fill_int_column(1, 10)
 t = create_table([c, c])
 a = group_by_sum(t, 0, [1])
-write_to_table(a, 'test_output/test.csv')
+write_to_csv(a, 'test_output/test.csv')
 assert same_content('test_output/test.csv', 'test_output/int10_1.csv')
