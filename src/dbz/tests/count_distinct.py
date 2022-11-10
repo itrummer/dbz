2f188a3777a59546b47557a15cdc7a0cbe7386ac
itrummer <immanuel.trummer@gmail.com>
2022-11-10 15:59:43 -0500

    Fixed test case of count distinct operator.

--- src/dbz/tests/count_distinct.py
+++ src/dbz/tests/count_distinct.py
@@ -6,5 +6,12 @@ Created on Nov 6, 2022
 c1 = fill_int_column(2, 10)
 c2 = fill_int_column(3, 10)
 t = create_table([c1, c1, c2])
-write_to_csv(t, 'test_output/test.csv')
+
+write_to_csv(count_distinct(t, [0]), 'test_output/test.csv')
+assert same_content('test_output/test.csv', 'test_output/int1_1x3.csv')
+
+write_to_csv(count_distinct(t, [0, 1]), 'test_output/test.csv')
+assert same_content('test_output/test.csv', 'test_output/int1_1x3.csv')
+
+write_to_csv(count_distinct(t, [0, 1, 2]), 'test_output/test.csv')
 assert same_content('test_output/test.csv', 'test_output/int1_1x3.csv')
