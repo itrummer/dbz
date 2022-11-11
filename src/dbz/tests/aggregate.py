21e3a18b5d9a1c5c7704a0d0373dce2459a69cec
itrummer <immanuel.trummer@gmail.com>
2022-11-10 22:51:00 -0500

    Encapsulate raw results in columns for aggregation test cases.

--- src/dbz/tests/aggregate.py
+++ src/dbz/tests/aggregate.py
@@ -5,6 +5,7 @@ Created on Nov 10, 2022
 '''
 ca = fill_float_column(1.5, 5)
 ag = calculate_<SubstituteBy:max|min|avg>(ca)
-t = create_table([ag])
+rc = fill_float_column(ag, 1)
+t = create_table([rc])
 write_to_csv(t, 'test_output/test.csv')
 assert same_content('test_output/test.csv', 'test_output/float1.5_1.csv')
--- src/dbz/tests/sum.py
+++ src/dbz/tests/sum.py
@@ -3,8 +3,9 @@ Created on Nov 10, 2022
 
 @author: immanueltrummer
 '''
-ca = fill_float_column(1, 7)
+ca = fill_int_column(1, 7)
 ag = calculate_sum(ca)
-t = create_table([ag])
+rc = fill_int_column(ag, 1)
+t = create_table([rc])
 write_to_csv(t, 'test_output/test.csv')
 assert same_content('test_output/test.csv', 'test_output/int7_1.csv')
