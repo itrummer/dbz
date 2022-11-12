192cdeb12c0f0b0d46ce45bf8c0808febcbbba49
itrummer <immanuel.trummer@gmail.com>
2022-11-12 12:55:08 -0500

    Added test case for rounding operator.

---
+++ src/dbz/tests/round.py
@@ -0,0 +1,10 @@
+'''
+Created on Nov 12, 2022
+
+@author: immanueltrummer
+'''
+c = fill_float_column(7.4, 10)
+r = round_column(c)
+t = create_table([r])
+write_to_csv(t, 'test_output/test.csv')
+assert same_content('test_output/test.csv', 'test_output/int7_10.csv')
