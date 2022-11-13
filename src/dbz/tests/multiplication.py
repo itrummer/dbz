a801fbd1eb447843822094b505595557a5bc930f
itrummer <immanuel.trummer@gmail.com>
2022-11-13 13:51:09 -0500

    Added test cases for arithmetic operators.

---
+++ src/dbz/tests/multiplication.py
@@ -0,0 +1,11 @@
+'''
+Created on Nov 13, 2022
+
+@author: immanueltrummer
+'''
+c1 = fill_int_column(1, 10)
+c2 = fill_int_column(7, 10)
+r = multiplication(c1, c2)
+t = create_table([r])
+write_to_csv(t, 'test_output/test.csv')
+assert same_content('test_output/test.csv', 'test_output/int7_10.csv')
