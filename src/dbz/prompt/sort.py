c0311b9ded676535158bb54eb7b0dd52f317cc85
itrummer <immanuel.trummer@gmail.com>
2022-11-12 19:20:46 -0500

    Simplified prompt for sort operator, adapted test cases for sorting.

--- src/dbz/prompt/sort.py
+++ src/dbz/prompt/sort.py
@@ -1,9 +1,7 @@
 def sort_rows(table, key_columns):
     """ Sort rows by key columns. 
     
-    1. Sort rows by key columns using following comparator:
-    - Compare values in key_columns order.
-    - <Null> values are sorted last.
+    1. Sort rows by key columns.
     2.<TablePost>
 
     Args:
--- src/dbz/tests/sort_2.py
+++ src/dbz/tests/sort_2.py
@@ -4,6 +4,6 @@ Created on Nov 6, 2022
 @author: immanueltrummer
 '''
 t = load_from_csv('test_input/test1.csv')
-s = sort_rows(t, [3])
+s = sort_rows(t, [1])
 write_to_csv(s, 'test_output/test.csv')
 assert same_content('test_output/test.csv', 'test_output/test1_sorted.csv')
