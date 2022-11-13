79e1974146073d65265ee71ce1cc9da5a4621cf7
itrummer <immanuel.trummer@gmail.com>
2022-11-13 15:26:05 -0500

    Fixed test case for substring operator.

--- src/dbz/tests/substring.py
+++ src/dbz/tests/substring.py
@@ -3,7 +3,7 @@ Created on Nov 13, 2022
 
 @author: immanueltrummer
 '''
-c = fill_string_column('###This is a test####')
+c = fill_string_column('###This is a test####', 10)
 r = substring(c, 4, 14)
 t = create_table([r])
 write_to_csv('test_output/test.csv')
