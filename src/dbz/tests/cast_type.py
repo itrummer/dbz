7a096269850bafc9ab9ad0f7bda257a14e2f4b02
itrummer <immanuel.trummer@gmail.com>
2022-11-13 14:08:51 -0500

    Added Boolean casting to casting tests.

--- src/dbz/tests/cast_type.py
+++ src/dbz/tests/cast_type.py
@@ -5,5 +5,5 @@ Created on Nov 6, 2022
 '''
 n = get_null()
 nc = fill_int_column(n, 10)
-r = <SubstituteBy:cast_to_int|cast_to_float|cast_to_string>(nc)
+r = <SubstituteBy:cast_to_Boolean|cast_to_int|cast_to_float|cast_to_string>(nc)
 assert check_column_type(r)
