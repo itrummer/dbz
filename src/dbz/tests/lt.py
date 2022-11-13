ae66454bfe066c636dc070df74b74c55d53df051
itrummer <immanuel.trummer@gmail.com>
2022-11-13 18:48:21 -0500

    Added test cases for binary equality/inequality comparisons.

---
+++ src/dbz/tests/lt.py
@@ -0,0 +1,17 @@
+'''
+Created on Nov 13, 2022
+
+@author: immanueltrummer
+'''
+c1 = fill_int_column(2, 10)
+c2 = fill_int_column(5, 10)
+
+r = less_than(c1, c2)
+t = create_table([r])
+write_to_csv(t, 'test_output/test.csv')
+assert same_content('test_output/test.csv', 'test_output/10true.csv')
+
+r = less_than(c2, c1)
+t = create_table([r])
+write_to_csv(t, 'test_output/test.csv')
+assert same_content('test_output/test.csv', 'test_output/10false.csv')
---
+++ src/dbz/tests/lteq.py
@@ -0,0 +1,22 @@
+'''
+Created on Nov 13, 2022
+
+@author: immanueltrummer
+'''
+c1 = fill_int_column(2, 10)
+c2 = fill_int_column(5, 10)
+
+r = less_than_or_equal(c1, c2)
+t = create_table([r])
+write_to_csv(t, 'test_output/test.csv')
+assert same_content('test_output/test.csv', 'test_output/10true.csv')
+
+r = less_than_or_equal(c2, c1)
+t = create_table([r])
+write_to_csv(t, 'test_output/test.csv')
+assert same_content('test_output/test.csv', 'test_output/10false.csv')
+
+r = less_than_or_equal(c1, c1)
+t = create_table([r])
+write_to_csv(t, 'test_output/test.csv')
+assert same_content('test_output/test.csv', 'test_output/10true.csv')
