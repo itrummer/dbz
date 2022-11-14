a4e442adbaf69e83e70c54484e79662fa3418300
itrummer <immanuel.trummer@gmail.com>
2022-11-14 16:21:44 -0500

    Added test case for set_column operator, changing column  data type.

---
+++ src/dbz/tests/set_column_2.py
@@ -0,0 +1,13 @@
+'''
+Created on Nov 14, 2022
+
+@author: immanueltrummer
+'''
+c1 = fill_int_column(1, 10)
+t1 = create_table([c1])
+
+c2 = fill_string_column('This is a test', 10)
+t2 = set_column(t1, 0, c2)
+
+write_to_csv(t2, 'test_output/test.csv')
+assert same_content('test_output/test.csv', 'test_output/string10.csv')
