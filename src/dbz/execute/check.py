1845d87557a50f4e988c744aba5a24d823110690
itrummer <immanuel.trummer@gmail.com>
2022-11-20 17:24:06 -0500

    Refined result comparison for SQL queries with order-by clause.

--- src/dbz/execute/check.py
+++ src/dbz/execute/check.py
@@ -11,7 +11,67 @@ import logging
 import math
 import os
 import pandas as pd
+import sqlglot.expressions
 import traceback
+
+
+class QueryInfo():
+    """ Extracts information for result verification from SQL query. """
+    
+    def __init__(self, query):
+        """ Extracts information from input query.
+        
+        Args:
+            query: SQL query
+        """
+        parsed = sqlglot.parse_one(query)
+        self.select_names = self._select_names(parsed)
+        self.order_names = self._order_names(parsed)
+    
+    def _name(self, item):
+        """ Extracts name for given item (e.g., a column). 
+        
+        Args:
+            item: an item that appears in a list clause
+        
+        Returns:
+            item name or empty string (if the name cannot be extracted)
+        """
+        if isinstance(item, sqlglot.expressions.Column):
+            return item.sql()
+        elif isinstance(item, sqlglot.expressions.Alias):
+            return item.args['alias'].sql()
+        else:
+            return ''
+    
+    def _order_names(self, parsed):
+        """ Extract names of items in order-by clause. 
+        
+        Args:
+            parsed: input query in tree representation
+        
+        Returns:
+            a list of item names
+        """
+        if not 'order' in parsed.args:
+            return []
+        
+        order_clause = parsed.args['order']
+        order_items = order_clause.args['expressions']
+        return [self._name(i.args['this']) for i in order_items]
+        
+    
+    def _select_names(self, parsed):
+        """ Extract names of items in select clause.
+        
+        Args:
+            parsed: tree representation of input query
+        
+        Returns:
+            a list of item names
+        """
+        select_items = parsed.args.get('expressions', [])
+        return [self._name(i) for i in select_items]
 
 
 class Validator():
@@ -46,20 +106,37 @@ class Validator():
         else:
             return self._code_check(engine, check)
     
-    def _check_row_order(self, check):
-        """ Determines whether check entails verifying the order of rows.
+    def _check_order_columns(self, check, nr_result_cols):
+        """ Returns columns to sort by before result comparison.
         
         Args:
-            check: a check comparing results of two engines
+            check: columns refer to this check
+            nr_result_cols: number of columns in result
         
         Returns:
-            True iff different row orders entail a failed comparison
+            a list of column indexes
         """
-        if check['type'] == 'sql':
+        check_type = check['type']
+        if not (check_type == 'sql'):
+            return []
+        else:
             query = check['query']
-            return 'order by' in query.lower()
-        
-        return False
+            query_info = QueryInfo(query)
+            # Sort by all columns if no order specified
+            order_names = query_info.order_names
+            if not order_names:
+                return list(nr_result_cols)
+            else:
+                # Do we know names of all select items?
+                select_names = query_info.select_names
+                if not (len(select_names) == nr_result_cols):
+                    return []
+                else:
+                    # Return select items that do not appear in order-by clause
+                    order_names = set(order_names)
+                    return [
+                        i for i in range(nr_result_cols) 
+                        if select_names[i] not in order_names]
 
     def _code_check(self, engine, check):
         """ Validate a test case specified as code. 
@@ -195,12 +272,19 @@ class Validator():
             check_path = self._write_rounded(check_df, check_path)
             ref_path = self._write_rounded(ref_df, ref_path)
             
-            if not self._check_row_order(check):
-                check_df.sort_values(
-                    list(check_df.columns), inplace=True, ignore_index=True)
-                ref_df.sort_values(
-                    list(ref_df.columns), inplace=True, ignore_index=True)
+            nr_check_cols = check_df.shape[1]
+            nr_ref_cols = ref_df.shape[1]
+            if not (nr_check_cols == nr_ref_cols):
+                print(f'Different nr. columns: {nr_check_cols}, {nr_ref_cols}')
+                return False
             
+            sort_cols = self._check_order_columns(check, nr_ref_cols)
+            if sort_cols:
+                check_df.sort_values(
+                    sort_cols, inplace=True, ignore_index=True)
+                ref_df.sort_values(
+                    sort_cols, inplace=True, ignore_index=True)
+
             try:
                 diffs = ref_df.compare(check_df, align_axis=0)
                 print(f'--- Differences ---\n{diffs}\n---')
@@ -238,36 +322,44 @@ class Validator():
 
 if __name__ == '__main__':
     
-    parser = argparse.ArgumentParser()
-    parser.add_argument('data_dir', type=str, help='Path to data directory')
-    parser.add_argument('lib_path', type=str, help='Path to library')
-    parser.add_argument('python', type=str, help='Command for Python invocation')
-    parser.add_argument('pg_db', type=str, help='Name of Postgres database')
-    parser.add_argument('pg_user', type=str, help='Name of Postgres user')
-    parser.add_argument('pg_pwd', type=str, help='Password for Postgres database')
-    parser.add_argument('pg_host', type=str, help='Path to Postgres host')
-    parser.add_argument('queries', type=str, help='Path to input queries')
-    args = parser.parse_args()
+    query = "SELECT c_custkey, c_name, SUM(l_extendedprice * (1 - l_discount)) AS revenue, c_acctbal, n_name, c_address, c_phone, c_comment FROM customer, orders, lineitem, nation WHERE c_custkey = o_custkey AND l_orderkey = o_orderkey AND o_orderdate >= date '1993-10-01' AND o_orderdate < date '1994-1-1' AND l_returnflag = 'R' AND c_nationkey = n_nationkey GROUP BY c_custkey, c_name, c_acctbal, c_phone, n_name, c_address, c_comment ORDER BY revenue DESC"
+    qi = QueryInfo(query)
+    print(qi.select_names)
+    print(qi.order_names)
+    validator = Validator(None, None)
+    sort_cols = validator._check_order_columns({'type':'sql', 'query':query}, 8)
+    print(sort_cols)
     
-    print(f'Comparing query results to Postgres on sample queries ...')
-    paths = dbz.util.DbzPaths(args.data_dir)
-    with open(args.lib_path) as file:
-        library = file.read()
-    dbz_engine = dbz.execute.engine.DbzEngine(
-        paths, library, args.python)
-    pg_engine = dbz.execute.engine.PgEngine(
-        args.pg_db, args.pg_user, 
-        args.pg_pwd, args.pg_host)
-    
-    queries = dbz.execute.query.load_queries(args.queries)
-    queries = [dbz.execute.query.simplify(q) for q in queries]
-    
-    validator = Validator(paths, pg_engine)
-    results = []
-    for query in queries:
-        check = {'query':query, 'type':'sql'}
-        success = validator.validate(dbz_engine, check)
-        results += [success]
-    
-    print(f'Checks Passed: {results}')
-    print(f'Validation success: {success}')
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('data_dir', type=str, help='Path to data directory')
+    # parser.add_argument('lib_path', type=str, help='Path to library')
+    # parser.add_argument('python', type=str, help='Command for Python invocation')
+    # parser.add_argument('pg_db', type=str, help='Name of Postgres database')
+    # parser.add_argument('pg_user', type=str, help='Name of Postgres user')
+    # parser.add_argument('pg_pwd', type=str, help='Password for Postgres database')
+    # parser.add_argument('pg_host', type=str, help='Path to Postgres host')
+    # parser.add_argument('queries', type=str, help='Path to input queries')
+    # args = parser.parse_args()
+    #
+    # print(f'Comparing query results to Postgres on sample queries ...')
+    # paths = dbz.util.DbzPaths(args.data_dir)
+    # with open(args.lib_path) as file:
+        # library = file.read()
+    # dbz_engine = dbz.execute.engine.DbzEngine(
+        # paths, library, args.python)
+    # pg_engine = dbz.execute.engine.PgEngine(
+        # args.pg_db, args.pg_user, 
+        # args.pg_pwd, args.pg_host)
+        #
+    # queries = dbz.execute.query.load_queries(args.queries)
+    # queries = [dbz.execute.query.simplify(q) for q in queries]
+    #
+    # validator = Validator(paths, pg_engine)
+    # results = []
+    # for query in queries:
+        # check = {'query':query, 'type':'sql'}
+        # success = validator.validate(dbz_engine, check)
+        # results += [success]
+        #
+    # print(f'Checks Passed: {results}')
+    # print(f'Validation success: {success}')
