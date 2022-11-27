f3bcf19737710ddc9d8a9317b479a6e17506f78f
itrummer <immanuel.trummer@gmail.com>
2022-11-27 18:11:13 -0500

    Added analysis of size of generated code.

--- src/dbz/analyze/statistics.py
+++ src/dbz/analyze/statistics.py
@@ -22,6 +22,7 @@ if __name__ == '__main__':
     nr_interventions = nr_calls-1
     
     stats = {}
+    # Analyzing breakdown by component
     for component, calls in history.items():
         nr_calls = len(calls)
         nr_calls_key = f'nr_calls_{component}'
@@ -29,9 +30,17 @@ if __name__ == '__main__':
         
         total_s = sum(call['total_s'] for call in calls)
         total_s_key = f'total_s_{component}'
-        stats[component] = total_s
+        stats[total_s_key] = total_s
+    
+    # Analyzing synthesized code
+    s_calls = history['synthesizer']
+    generated_chars = sum(len(c['completion']) for c in s_calls)
+    generated_lines = sum(c['completion'].count('\n') for c in s_calls)
+    processed_chars = sum(len(c['prompt']) for c in s_calls) + generated_chars
+    stats['generated_chars'] = generated_chars
+    stats['generated_lines'] = generated_lines
+    stats['processed_chars'] = processed_chars
     
     print(f'--- {args.engine_dir} ---')
-    print(f'Nr. calls: {nr_calls}')
-    print(f'Nr. interventions: {nr_interventions}')
-    print(f'Statistics: {stats}')
+    for k, v in stats.items():
+        print(f'{k}\t:{v}')
