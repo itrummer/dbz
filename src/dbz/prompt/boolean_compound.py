05b8d6dae6d9918b2ecf4faeccf5f7e7195df2b5
itrummer <immanuel.trummer@gmail.com>
2023-10-12 22:49:26 -0400

    Refined description of Boolean operators.

--- src/dbz/prompt/boolean_compound.py
+++ src/dbz/prompt/boolean_compound.py
@@ -1,5 +1,5 @@
 def logical_<operator>(column_1, column_2):
-    """ Performs logical <operator>.
+    """ Performs logical <operator> (using ternary logic: <Null>, True, and False).
     
     Args:
         column_1: <Column> with values of type <BooleanField> or <Null>.
